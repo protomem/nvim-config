4864eb7d5f4ee70ea3aedf0f48072adb3085a4db
protomem <protomem0709@gmail.com>
2024-09-13 20:51:28 +0300

    update cpp config

--- lua/plugins/cpp.lua
+++ lua/plugins/cpp.lua
@@ -1,12 +1,16 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      setup = {
-        clangd = function(_, opts)
-          opts.capabilities.offsetEncoding = { "utf-16" }
-        end,
-      },
-    },
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.formatting.clang_format,
+      })
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "clang-format" } },
   },
 }
