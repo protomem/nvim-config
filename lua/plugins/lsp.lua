a132f19229dc1c2f0cf611983c39a4b1a86275c4
protomem <protomem0709@gmail.com>
2024-09-14 16:34:58 +0300

    update ui settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,4 +5,16 @@ return {
       inlay_hints = { enabled = false },
     },
   },
+
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        lsp_doc_border = true,
+      },
+    },
+  },
 }
