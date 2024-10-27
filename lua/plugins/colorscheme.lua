e9c588783637667750d48625da6648d61c614084
protomem <protomem0709@gmail.com>
2024-10-27 11:49:05 +0300

    update theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,9 @@
 return {
-  "projekt0n/github-nvim-theme",
-  name = "github-theme",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
   config = function()
-    require("github-theme").setup({
-      options = {
-        transparent = true,
-      },
-    })
-
-    vim.cmd("colorscheme github_dark_default")
+    vim.cmd("colorscheme solarized-osaka")
   end,
 }
