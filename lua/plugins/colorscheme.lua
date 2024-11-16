9dee87d6bb0968c141b0dc4262e430b97d5f8568
protomem <protomem0709@gmail.com>
2024-11-16 17:02:56 +0300

    update theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,26 @@
 return {
-  "projekt0n/github-nvim-theme",
-  name = "github-theme",
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require("github-theme").setup({
-      options = {
-        -- transparent = true,
-        -- im_inactive = true
-      },
-    })
+  -- "projekt0n/github-nvim-theme",
+  -- name = "github-theme",
+  -- lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  -- priority = 1000, -- make sure to load this before all the other start plugins
+  -- config = function()
+  --   require("github-theme").setup({
+  --     options = {
+  --       -- transparent = true,
+  --       -- im_inactive = true
+  --     },
+  --   })
+  --
+  --   vim.cmd("colorscheme github_dark_default")
+  -- end,
 
-    vim.cmd("colorscheme github_dark_default")
-  end,
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme moonfly")
+    end,
+  },
 }
